1621c922a7b38bf0b4aae44e2a98edd446ce9a56
juliosalvat <code@juliosalvat.com>
2014-10-09 16:24:41 -0700

    Lesson 3/assignment/2

---
+++ Contacts/Contacts/ContactDetails.swift
@@ -0,0 +1,35 @@
+//
+//  ContactDetails.swift
+//  Contacts
+//
+//  Created by able on 10/9/14.
+//  Copyright (c) 2014 able. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetails: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Contacts/Contacts/Contacts.swift
+++ Contacts/Contacts/Contacts.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class Contacts: UITableViewController, UITableViewDataSource, UITableViewDelegate {
     
+    //Declaring elements
+    struct contactInfo {
+        var name: String
+        var phoneNumber: String
+    }
+    
+    var listOfContacts: [contactInfo] = []
+    
+    //Sample contacts
+    var firstContact = contactInfo(name: "John Coffey" , phoneNumber: "(111) 111-1111")
+    var secondContact = contactInfo(name: "Cathy Kane" , phoneNumber: "(222) 222-2222")
+    
+    //TableView delegates
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 10
+        return listOfContacts.count
     }
     
     
@@ -20,8 +33,18 @@ class Contacts: UITableViewController, UITableViewDataSource, UITableViewDelegat
     
     let cell = tableView.dequeueReusableCellWithIdentifier("contact", forIndexPath: indexPath) as UITableViewCell
     
-    cell.textLabel!.text = "test"
+        cell.textLabel!.text = listOfContacts[indexPath.row].name
+        cell.detailTextLabel!.text = listOfContacts[indexPath.row].phoneNumber
+        
     return cell
-    
-}
+        
+    }
+        //ViewController lifeCycle
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            listOfContacts.append(firstContact)
+            listOfContacts.append(secondContact)
+        }
+
 }
