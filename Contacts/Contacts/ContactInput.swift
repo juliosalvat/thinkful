ad8eeb0a4a0443937aad965d5fb5d75a8d029e68
juliosalvat <code@juliosalvat.com>
2014-12-02 18:04:21 -0800

    update

---
+++ Contacts/Contacts/ContactInput.swift
@@ -0,0 +1,35 @@
+//
+//  ContactInput.swift
+//  Contacts
+//
+//  Created by able on 10/13/14.
+//  Copyright (c) 2014 able. All rights reserved.
+//
+
+import UIKit
+
+class ContactInput: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS Apps/jSonSkillz/bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  jSonSkillz
+//
+//  Created by skooliostyles on 11/7/14.
+//  Copyright (c) 2014 JulioSalvat. All rights reserved.
+//
+
+#ifndef jSonSkillz_bridge_h
+#define jSonSkillz_bridge_h
+
+
+#endif
