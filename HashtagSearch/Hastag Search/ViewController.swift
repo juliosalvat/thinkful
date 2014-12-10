f43b62a6e42b51bdd4f4c75be911b590f95a2026
juliosalvat <code@juliosalvat.com>
2014-12-10 11:16:39 -0800

    hashtagProject2

--- HashtagSearch/Hastag Search/ViewController.swift
+++ HashtagSearch/Hastag Search/ViewController.swift
@@ -5,23 +5,23 @@
 //  Created by skooliostyles on 12/3/14.
 //  Copyright (c) 2014 TechShock. All rights reserved.
 //
-import Foundation
 import UIKit
-
 class ViewController: UIViewController, UISearchBarDelegate {
     
-    let instagramClientID = "8f3108299a724187af377ef9e4dc856b"
-
     @IBOutlet weak var scrollView: UIScrollView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let instagramClientID = "8f3108299a724187af377ef9e4dc856b"
+    
+    func searchInstagramByHashtag(searchString: String) {
+        for subview in self.scrollView.subviews {
+            subview.removeFromSuperview()
+        }
         
+        let instagramURLString = "https://api.instagram.com/v1/tags/" + searchString + "/media/recent?client_id=" + instagramClientID
         
         let manager = AFHTTPRequestOperationManager()
         
-        manager.GET( "https://api.instagram.com/v1/tags/clararockmore/media/recent?client_id=8f3108299a724187af377ef9e4dc856b",
+        manager.GET( instagramURLString,
             parameters: nil,
             success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
                 println("JSON: " + responseObject.description)
@@ -44,45 +44,24 @@ class ViewController: UIViewController, UISearchBarDelegate {
             failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
                 println("Error: " + error.localizedDescription)
         })
-        
     }
     
-        func searchBarSearchButtonClicked(searchBar: UISearchBar!) {
-            for subview in self.scrollView.subviews {
-                subview.removeFromSuperview()
-            }
-            
-            searchBar.resignFirstResponder()
-            
-            let instagramURLString = "https://api.instagram.com/v1/tags/" + searchBar.text + "/media/recent?client_id=8f3108299a724187af377ef9e4dc856b"
-            
-            let manager = AFHTTPRequestOperationManager()
-            
-            manager.GET( instagramURLString,
-                parameters: nil,
-                success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
-                    println("JSON: " + responseObject.description)
-                    
-                    if let dataArray = responseObject.valueForKey("data") as? [AnyObject] {
-                        self.scrollView.contentSize = CGSizeMake(320, CGFloat(320*dataArray.count))
-                        for var i = 0; i < dataArray.count; i++ {
-                            let dataObject: AnyObject = dataArray[i]
-                            if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
-                                println("image " + String(i) + " URL is " + imageURLString)
-                                
-                                let imageData =  NSData(contentsOfURL: NSURL(string: imageURLString)!)
-                                let imageView = UIImageView(image: UIImage(data: imageData!))
-                                imageView.frame = CGRectMake(0, CGFloat(320*i), 320, 320)
-                                self.scrollView.addSubview(imageView)
-                            }
-                        }
-                    }
-                },
-                failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
-                    println("Error: " + error.localizedDescription)
-            })
-        }
+    func searchBarSearchButtonClicked(searchBar: UISearchBar!) {
+        
+        searchBar.resignFirstResponder()
+        searchInstagramByHashtag(searchBar.text)
+    }
     
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        searchInstagramByHashtag("clararockmore")
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
+
 
